3f2c5cbd0979940c7cebb5e4dd4d10463c5dfe84
gnaturanitechsol <34765842+gnaturanitechsol@users.noreply.github.com>
2019-02-12 13:51:01 +0100

    Add files via upload

---
+++ webapp/controller/Root.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("it.marcegaglia.mm.purchaserequestPurchaseRequestStep.controller.Root", {
+
+	});
+});
